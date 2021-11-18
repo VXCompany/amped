b8e0c0a292d7f635f06cd2aca8f2d99c4fde035d
Aris van Ommeren <avommeren@vxcompany.com>
2021-11-18 13:40:50 +0100

    Add Read flag

--- Code/Amped.API.Tests/UnitTest1.cs
+++ Code/Amped.API.Tests/UnitTest1.cs
@@ -25,15 +25,25 @@ namespace Amped.API.Tests
 
             bookmark.Uri.Should().Be(expected);
         }
-        
-        
+
+        [Fact]
+        public void Should_Set_Read()
+        {
+            Uri uri = new Uri("http://someuri.com");
+            var read = true;
+
+            var bookmark = new Bookmark(uri, read);
+
+            bookmark.Read.Should().Be(true);
+        }
     }
 
     public class Bookmark
     {
         public Uri Uri { get; set; }
+        public object Read { get; internal set; }
 
-        public Bookmark(Uri uri)
+        public Bookmark(Uri uri, bool read = false)
         {
             Uri = uri ?? throw new ArgumentNullException();
         }
