00bfaac3953a97201ed56409b85a2b9dcaa5c83c
Dimitri van Leent <dvleent@vxcompany.com>
2021-11-18 13:52:48 +0100

    test voor parker

--- Code/Amped.API.Tests/BookmarkTests.cs
+++ Code/Amped.API.Tests/BookmarkTests.cs
@@ -6,12 +6,14 @@ namespace Amped.API.Tests
 {
     public class BookmarkTests
     {
+        private const string DummyOwner = "dummy owner";
+
         [Fact]
         public void Should_Throw_ArgumentNullException_When_No_Uri()
         {
             Uri invalidUri = null;
             
-            Action bookmark = () => new Bookmark(invalidUri);
+            Action bookmark = () => new Bookmark(invalidUri, DummyOwner);
 
             bookmark.Should().Throw<ArgumentNullException>();
         }
@@ -21,7 +23,7 @@ namespace Amped.API.Tests
         {
             var expected = new Uri("http://someuri.com");
             
-            var bookmark = new Bookmark(expected);
+            var bookmark = new Bookmark(expected, DummyOwner);
 
             bookmark.Uri.Should().Be(expected);
         }
@@ -32,15 +34,21 @@ namespace Amped.API.Tests
             var uri = new Uri("http://someuri.com");
             var read = true;
 
-            var bookmark = new Bookmark(uri, read);
+            var bookmark = new Bookmark(uri, DummyOwner, read);
 
             bookmark.Read.Should().Be(true);
         }
 
         [Fact]
-        public void METHOD()
+        public void Should_Specify_Whos_Bookmark_It_Is()
         {
-            
+            var uri = new Uri("http://someuri.com");
+            var read = true;
+            var parker = "parker";
+
+            var bookmark = new Bookmark(uri, parker, read);
+
+            bookmark.Owner.Should().Be(parker);
         }
 
     }
--- Code/Amped.API/Bookmark.cs
+++ Code/Amped.API/Bookmark.cs
@@ -6,10 +6,12 @@ namespace Amped.API.Tests
     {
         public Uri Uri { get; }
         public bool Read { get; }
+        public object Owner { get; }
 
-        public Bookmark(Uri uri, bool read = false)
+        public Bookmark(Uri uri, string owner, bool read = false)
         {
             Uri = uri ?? throw new ArgumentNullException();
+            Owner = owner;
             Read = read;
         }
     }
