b67af62231cd5b360df5f074e35bec2b3b1965af
Albert Starreveld <astarreveld@vxcompany.com>
2021-11-18 13:31:07 +0100

    feat(bookmarks): added bookmark class

--- Code/Amped.API.Tests/UnitTest1.cs
+++ Code/Amped.API.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System;
+using FluentAssertions;
 using Xunit;
 
 namespace Amped.API.Tests
@@ -5,8 +7,35 @@ namespace Amped.API.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Should_Throw_ArgumentNullException_When_No_Uri()
         {
+            Uri invalidUri = null;
+            
+            Action bookmark = () => new Bookmark(invalidUri);
+
+            bookmark.Should().Throw<ArgumentNullException>();
+        }
+        
+        [Fact]
+        public void Should_Set_Uri()
+        {
+            Uri expected = new Uri("http://someuri.com");
+            
+            var bookmark = new Bookmark(expected);
+
+            bookmark.Uri.Should().Be(expected);
+        }
+        
+        
+    }
+
+    public class Bookmark
+    {
+        public Uri Uri { get; set; }
+
+        public Bookmark(Uri uri)
+        {
+            Uri = uri ?? throw new ArgumentNullException();
         }
     }
 }
