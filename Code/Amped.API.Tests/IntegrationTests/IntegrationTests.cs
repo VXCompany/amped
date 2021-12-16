d88bf1588c94e63a0b9dd4705d954a66dfc1b6fe
Daan Stolp <mail@daanstolp.nl>
2021-12-16 15:19:28 +0100

    Start building an integration test.

---
+++ Code/Amped.API.Tests/IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Amped.API.Tests.IntegrationTests
+{
+    public class TestFixture : IDisposable
+    {
+        private readonly HttpClient _client;
+        
+        public TestFixture()
+        {
+            var factory = new WebApplicationFactory<Startup>();
+            _client = factory.CreateClient();
+        }
+        
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    } 
+    
+    public class IntegrationTests
+    {
+        public IntegrationTests(TestFixture fixture)
+        {
+            
+        }
+
+        [Fact]
+        public void Can_create_a_bookmark()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.PostAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+    }
+}
--- Code/Amped.API/Controllers/IBookmarkRepository.cs
+++ Code/Amped.API/Controllers/IBookmarkRepository.cs
@@ -4,4 +4,12 @@
     {
         void Add(Bookmark bookmark);
     }
+
+    public class BookmarkRepository : IBookmarkRepository
+    {
+        public void Add(Bookmark bookmark)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
