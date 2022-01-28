797526276edf4ab9aa675233e07c1f7482da9a47
Albert Starreveld <astarreveld@vxcompany.com>
2022-01-28 10:08:58 +0100

    feat(contract-testing): Added endpoint to list all bookmarks.

---
+++ Code/Amped.API.Tests/IntegrationTests/ApiTestClientBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+
+namespace Amped.API.Tests.IntegrationTests
+{
+    public class ApiTestClientBuilder
+    {
+        public HttpClient Build()
+        {
+            var factory = new WebApplicationFactory<Startup>();
+            return factory.CreateClient();
+        }
+    }
+}
