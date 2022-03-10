79832ebd61ef5ab5ce02a0a3a585bb7e05997342
Dimitri van Leent <dimi82@gmail.com>
2022-03-10 15:44:08 +0100

    svelte start

--- App/src/App.svelte
+++ App/src/App.svelte
@@ -1,10 +1,23 @@
 <script>
+import { onMount } from "svelte";
+import BookmarkList from './BookmarkList.svelte';
+import CreateBookmark from './CreateBookmark.svelte';
+
 	export let name;
+	let bookmarks = []
+
+	onMount(async () => {
+		const res = await fetch("http://localhost:5001/api/Bookmark/all");
+		bookmarks = await res.json();
+	})
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Ik wil een front-end met leuke kleurtjes!</p>
+
+	<BookmarkList bookmarks={bookmarks} />
+	<CreateBookmark/>
 </main>
 
 <style>
