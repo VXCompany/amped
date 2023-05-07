cf8534cde4ba7c843595f6198039c0ae5e81d99e
Yuri Burger <yuriburger@outlook.com>
2023-05-07 20:56:01 +0200

    Updated validator

--- Bookmarks/App/src/validators.js
+++ Bookmarks/App/src/validators.js
@@ -1,16 +1,16 @@
-function uriValidator () {
-    return function uri (value) {
-      return (value && value.startsWith('http')) || 'Please enter a valid uri'
+function uriValidator() {
+    return function uri(value) {
+        return (value && value.startsWith('http')) || 'Please enter a valid uri'
     }
-  }
-  
-  function requiredValidator () {
-    return function required (value) {
-      return (value !== undefined && value !== null && value !== '') || 'This field is required'
+}
+
+function requiredValidator() {
+    return function required(value) {
+        return (value !== undefined && value !== null && value !== '') || 'This field is required'
     }
-  }
-  
-  export {
+}
+
+export {
     uriValidator,
     requiredValidator
-  }
+}
