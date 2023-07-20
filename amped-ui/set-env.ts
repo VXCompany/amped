ae7787f13f78934ec87cb6d8689000141c42851b
Yuri Burger <yuriburger@outlook.com>
2023-07-20 14:41:48 +0200

    Generate prod bundle

--- amped-ui/set-env.ts
+++ amped-ui/set-env.ts
@@ -1,15 +1,30 @@
 const { writeFile } = require('fs');
 const { promisify } = require('util');
 const dotenv = require('dotenv');
+const { argv } = require('yargs');
 
 dotenv.config();
 
+const environment = argv.environment;
+const isProduction = environment === 'production';
+
+if (!process.env['API_BOOKMARK_URL'] || !process.env['API_PROFILE_URL'] ||
+    !process.env['AUTH0_DOMAIN'] || !process.env['AUTH0_CLIENT_ID'] ||
+    !process.env['AUTH0_AUDIENCE'] || !process.env['AUTH0_CALLBACK_URL']) 
+    {
+      console.error('Not all the required environment variables were provided!');
+      process.exit(-1);
+}
+
+
 const writeFilePromisified = promisify(writeFile);
 
-const targetPath = './src/environments/environment.ts';
+const targetPath = isProduction
+   ? `./src/environments/environment.prod.ts`
+   : `./src/environments/environment.ts`;
 
 const envConfigFile = `export const environment = {
-  production: false,
+  production: ${isProduction},
   auth0: {
     domain: '${process.env['AUTH0_DOMAIN']}',
     clientId: '${process.env['AUTH0_CLIENT_ID']}',
